b7196126d397e7b3d93b82335027dc4be7689a34
FatherofElias <supra_3_8@hotmail.com>
2024-12-29 12:14:18 -0600

    uspate

--- app_test.py
+++ app_test.py
@@ -1,10 +1,8 @@
 import unittest
 
-
-class TestSumFunction(unittest.TestCase):
+class TestNegativeSum(unittest.TestCase):
     def test_negative_sum(self):
-        result = sum_function(-1, -1)
-        self.assertEqual(result, -2)
+        self.assertEqual(-1 + -1, -2)
 
 if __name__ == '__main__':
     unittest.main()
