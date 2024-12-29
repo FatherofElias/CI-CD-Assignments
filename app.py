17e7fc6e3770887fd95335a6fffe73f6b3d14fb3
FatherofElias <supra_3_8@hotmail.com>
2024-12-29 15:57:40 -0600

    update

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask!"
 
+@app.route('/sum')
+def sum_example():
+    return str(-1 + -1)
+
+
 #if __name__ == '__main__':
  #   app.run(debug=True)
