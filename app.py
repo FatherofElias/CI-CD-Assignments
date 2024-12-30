5c5ef7facc99f70d5f3867bbc58956ad82aef319
FatherofElias <supra_3_8@hotmail.com>
2024-12-30 15:54:57 -0600

    update

--- app.py
+++ app.py
@@ -5,13 +5,11 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://example_sum_postgres_2moj_user:lcOZ8DlDUF3Mu2ZnQLm3Xcq9AGnqlJFk@dpg-ctpfkrrqf0us73ebldcg-a.ohio-postgres.render.com/example_sum_postgres_2moj'
 db = SQLAlchemy(app)
 
-
 class SumResult(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     a = db.Column(db.Integer, nullable=False)
     b = db.Column(db.Integer, nullable=False)
     result = db.Column(db.Integer, nullable=False)
-
 
 @app.route('/sum', methods=['POST'])
 def add_sum():
@@ -24,23 +22,19 @@ def add_sum():
     db.session.commit()
     return jsonify({"a": a, "b": b, "result": result}), 201
 
-
 @app.route('/sum', methods=['GET'])
 def get_all_sums():
     sums = SumResult.query.all()
     result_list = [{"a": sum_result.a, "b": sum_result.b, "result": sum_result.result} for sum_result in sums]
     return jsonify(result_list), 200
 
-
 @app.route('/sum/result/<int:result_value>', methods=['GET'])
 def get_sums_by_result(result_value):
     sums = SumResult.query.filter_by(result=result_value).all()
     if not sums:
         return jsonify({"error": "No sums found with the specified result"}), 404
-
     result_list = [{"a": sum_result.a, "b": sum_result.b, "result": sum_result.result} for sum_result in sums]
     return jsonify(result_list), 200
-
 
 if __name__ == '__main__':
     app.run(debug=True)
